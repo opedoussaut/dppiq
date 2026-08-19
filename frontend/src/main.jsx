06500285c58473cf4eb4642c4e0b43fb9531e11c
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-08-19 12:00:16 +0200

    Load REGIQ score interaction styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './yuka.css'
 
 if ('serviceWorker' in navigator && (window.isSecureContext || location.hostname === 'localhost')) {
   window.addEventListener('load', () => {
