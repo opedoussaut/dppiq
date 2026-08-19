9b32921a7fc19ab759d6fd8d2e86088dfb161001
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-08-19 06:57:37 +0200

    Add minimal PWA service worker

---
+++ frontend/public/sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'dppiq-shell-v1'
+const SHELL = ['/', '/manifest.webmanifest', '/dppiq-icon.svg']
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).catch(() => undefined))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(hit => hit || caches.match('/')))
+  )
+})
