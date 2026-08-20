303054fbc9fe4f06e85604b3d4bac6639e0c6228
Olivier Pédoussaut <opedoussaut@gmail.com>
2026-08-20 15:57:01 +0200

    Fix dev service-worker cache cleanup

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -8,7 +8,7 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('regiq-shell-') && key !== CACHE).map(key => cache.delete ? cache.delete(key) : caches.delete(key))))
+    caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('regiq-shell-') && key !== CACHE).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   )
 })
